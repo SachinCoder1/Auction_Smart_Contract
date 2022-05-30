0c83c3f402cd123c147d8d534ec299a7550a5b18
sachincoder1 <sachinhlo232@gmail.com>
2022-05-30 11:06:10 +0530

    cancel auction and constructor

--- Auction.sol
+++ Auction.sol
@@ -11,17 +11,56 @@ contract Auction {
 
     uint public highestBid;
     uint public highestPayableBid;
-    uint public bidInc;
+    uint public bidAmount;
 
     address payable public highestBidder;
 
     mapping(address => uint) public bidders;
 
+
+    //  Constructor
+    constructor(){
+        owner = payable(msg.sender);
+
+        auctionState = State.Running;
+
+        startTime = block.number;
+        endTime = startTime + 240;
+
+        bidAmount = 1 ether;
+    }
+
+
     // modifier for not owner;
-    modifier isOwner {
+    modifier onlyOwner {
+        require(msg.sender == owner, "Sorry you are not permitted to use it.");
+        _;
+    }
+    modifier notOwner {
         require(msg.sender != owner, "Owner Can't bid");
         _;
     }
+
+    // modifier for the auction is started.
+    modifier started {
+        require(block.number > startTime);
+        _;
+    }
+
+    // modifier for the auction is ended.
+    modifier isEnded {
+        require(block.number <= endTime);
+        _;
+    }
+
+
+    // to cancel the auction
+    function cancelAuction() public onlyOwner {
+        auctionState = State.Cancelled;
+    }
+
+
+
 
 
 
